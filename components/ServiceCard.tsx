40cf8034a64309183a08e09495719882e8f5f2c3
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-02 15:17:16 +0200

    styling about me

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -4,12 +4,18 @@ import { IService } from "../type";
 const ServiceCard: FunctionComponent<{ service: IService }> = ({
   service: { Icon, title, about },
 }) => {
+  const createMarkup = () => {
+    return {
+      __html: about,
+    };
+  };
+
   return (
     <div className="flex items-center p-2 space-x-4 ">
       <Icon className="w-12 h-12 text-green" />
       <div className="">
-        <h6 className="font-bold">{title}</h6>
-        <p>{about}</p>
+        <h4 className="font-bold">{title}</h4>
+        <p dangerouslySetInnerHTML={createMarkup()} />
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import ServiceCard from "../components/ServiceCard";
 
 const index = () => {
   return (
-    <div className="flex flex-col px-6 pt-1">
+    <div className="flex flex-col flex-grow px-6 pt-1">
       <h5 className="my-3 font-medium">
         My name is <b>Stanisław</b>! I am a freshman at{" "}
         <i>
@@ -25,11 +25,16 @@ const index = () => {
         experiment with creating new code! I am motivated and ready to work hard
         and develop my coding skills.
       </h5>
-      <div className="p-4 mt-5 bg-gray-400">
-        <h6>What I Offer</h6>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400"
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
         <div className="grid gap-6 lg:grid-cols-2">
           {services.map((service) => (
-            <ServiceCard service={service} />
+            <div className="bg-gray-200 rounded-lg lg:col-span-1 ">
+              <ServiceCard service={service} />
+            </div>
           ))}
         </div>
       </div>
