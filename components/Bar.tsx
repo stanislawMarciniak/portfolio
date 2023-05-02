d2637475d85afc8d3fed23f8b0267539f5afd8ef
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-02 18:47:35 +0200

    languages & tools

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -4,7 +4,14 @@ import React, { FunctionComponent } from "react";
 const Bar: FunctionComponent<{
   data: ISkill;
 }> = ({ data: { Icon, name } }) => {
-  return <div>{name}</div>;
+  return (
+    <div className="my-2 text-white bg-gray-300 rounded-full">
+      <div className="flex items-center px-4 py-1 rounded-full flex-center bg-gradient-to-r from-green to-blue-600">
+        <Icon className="mr-3 " />
+        {name}
+      </div>
+    </div>
+  );
 };
 
 export default Bar;
