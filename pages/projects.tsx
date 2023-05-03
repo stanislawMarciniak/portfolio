1a56de06cdf87a5d33a09859baeeba97c63ea2ee
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-04 01:33:02 +0200

    edit to build

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,8 +5,11 @@ const Projects = () => {
   return (
     <div className="relative grid grid-cols-12 gap-4 my-3">
       {projects.map((project) => (
-        <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-6 dark:bg-dark-200">
-          <ProjectCard project={project} key={project.name} />
+        <div
+          key={project.name}
+          className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-6 dark:bg-dark-200"
+        >
+          <ProjectCard project={project} />
         </div>
       ))}
     </div>
