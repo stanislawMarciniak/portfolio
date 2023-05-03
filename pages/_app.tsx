6c2c872ca55cb5dd22e9c9d03f4ef99cee5ce394
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-04 01:24:47 +0200

    debugging darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ThemeProvider } from "next-themes";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
           <Sidebar />
@@ -15,6 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
