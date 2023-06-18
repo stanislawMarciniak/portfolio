341f0db2f335a7d0a0d2bf5ae0c368ce3c312f0b
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-06-18 14:38:52 +0200

    visual portfolio merge

--- src/components/visual-portfolio/Portfolio.js
+++ src/components/visual-portfolio/Portfolio.js
@@ -1,23 +1,20 @@
-import './Portfolio.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
 
 export function Portfolio() {
   return (
-    <div className="App">
+    <div className="portfolio">
       <NavBar />
       <Banner />
       <Skills />
       <Projects />
       <Contact />
-      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Portfolio;
