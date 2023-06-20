4c762fb062b6afd120be704df980d658541198cd
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-06-20 22:26:08 +0200

    responsive projects

--- src/components/visual-portfolio/components/ProjectCard.js
+++ src/components/visual-portfolio/components/ProjectCard.js
@@ -1,39 +1,46 @@
 import Image from "next/image";
+import { useMediaQuery } from "@mui/material";
 
 export const ProjectCard = ({ project, index }) => {
+  const isSmallerThanLg = useMediaQuery((theme) =>
+    theme.breakpoints.down("lg")
+  );
+
   return (
     <div>
-      {!(index % 2) ? (
-        <div className="flex justify-around mb-32 ">
-          <Image
-            className="rounded-3xl"
-            layout="intrinsic"
-            width={633}
-            height={310}
-            src={project.image_path}
-            alt=""
-          />
-          <div className="w-1/3">
-            <h4>{project.name}</h4>
-            <span>{project.description}</span>
-          </div>
+      {index % 2 || isSmallerThanLg ? (
+        <div className="justify-between gap-6 mb-32 lg:flex ">
+          <DescriptionAndHeader project={project} />
+          <ProjectImage project={project} />
         </div>
       ) : (
-        <div className="flex justify-around mb-32">
-          <div className="w-1/3">
-            <h4>{project.name}</h4>
-            <span>{project.description}</span>
-          </div>
-          <Image
-            className="rounded-3xl"
-            layout="intrinsic"
-            width={633}
-            height={310}
-            src={project.image_path}
-            alt=""
-          />
+        <div className="justify-between gap-6 mb-32 lg:flex ">
+          <ProjectImage project={project} />
+          <DescriptionAndHeader project={project} />
         </div>
       )}
+    </div>
+  );
+};
+
+const ProjectImage = ({ project }) => {
+  return (
+    <Image
+      className="rounded-3xl"
+      layout="intrinsic"
+      width={792}
+      height={388}
+      src={project.image_path}
+      alt=""
+    />
+  );
+};
+
+const DescriptionAndHeader = ({ project }) => {
+  return (
+    <div className="mb-3 lg:w-1/3">
+      <h4>{project.name}</h4>
+      <span>{project.description}</span>
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import Head from "next/head";
 import { wrapper } from "../redux/store";
 import { useSelector } from "react-redux";
 import { selectFoldedState } from "../redux/folder";
-import { Portfolio } from "../components/visual-portfolio/Portfolio"
+import { Portfolio } from "../components/visual-portfolio/Portfolio";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
@@ -13,6 +14,18 @@ const App = ({ Component, pageProps }) => {
   const onClickAnywhere = () => {
     inputRef.current.focus();
   };
+
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 640,
+        md: 768,
+        lg: 1024,
+        xl: 1280,
+      },
+    },
+  });
 
   return (
     <>
@@ -32,22 +45,22 @@ const App = ({ Component, pageProps }) => {
 
         `}
       </style>
-      {folded ? 
-        <div className="text-dark-foreground">
-          <Portfolio />
-        </div> :
-      
-        <div
-          className='flex items-center justify-center text-xs text-dark-foreground min-w-max md:min-w-full md:text-base'
-          onClick={onClickAnywhere}
-        >
-          <main
-            className='w-2/3 px-1 py-1 shadow-2xl bg-dark-background rounded-2xl h-5/6'
+      <ThemeProvider theme={theme}>
+        {folded ? (
+          <div className="text-dark-foreground">
+            <Portfolio />
+          </div>
+        ) : (
+          <div
+            className="flex items-center justify-center text-xs text-dark-foreground min-w-max md:min-w-full md:text-base"
+            onClick={onClickAnywhere}
           >
-            <Component {...pageProps} inputRef={inputRef} />
-          </main>
-        </div>
-           }
+            <main className="w-2/3 px-1 py-1 shadow-2xl bg-dark-background rounded-2xl h-5/6">
+              <Component {...pageProps} inputRef={inputRef} />
+            </main>
+          </div>
+        )}
+      </ThemeProvider>
     </>
   );
 };
